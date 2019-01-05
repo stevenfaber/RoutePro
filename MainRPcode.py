2a2434e81d313ae5cc2b33f0fb79dba43c8e74bc
tgupta5 <tgupta5@illinois.edu>
2019-01-05 18:14:00 -0500

    minor syntactical changes - algo/chat Sunday

--- MainRPcode.py
+++ MainRPcode.py
@@ -6,43 +6,53 @@ api_key = 'AIzaSyB23T32B3CyuMJNUD30OA-V7qfCeL8GcUE'
 
 
 def main():
-    '''
+    """
     main function which will execute all other functions in program to perform task
     :input: none
-    :return: none, should generate two web pages (directions to CTA, directions to destination from CTA)
-    '''
+    :return: Generate two web pages (directions to CTA, directions to destination from CTA)
+    """
     # establishes start end and time data
     query_user()
     # returns optimal subway stop
     subway_stop_location = sort_routes()
     # returns the time you need to arrive at the CTA
     arrival_time_transit = determine_arrival_time(subway_stop_location)
-    # launches directions in google mamps, with two windows for directions to and from CTA
+    # launches directions in google maps, with two windows for directions to and from CTA
     launch_directions(arrival_time_transit, subway_stop_location)
 
 
 def query_user():
+    """
+    query_user() function has pre-assigned variables above for testing purposes
+    :input: none
+    :return:
+    """
+
     global destination, origin, arrival_time
-    '''
 
-    query_user() function has pre-assigned varibales above for testing purposes
-
-    destination = input('What is the location of your event?').replace(" ","+")
-    origin = input('Where will you be traveling from?').replace(" ","+")
-    pattern = '%m.%d.%Y %H:%M'
-    date_time = input('What day and time will the event be? Militaty Time(MM.DD.YYYY HH:MM)')
-    arrival_time = int(time.mktime(time.strptime(date_time, pattern)))
-    '''
     arrival_time = 1547235000
     origin = '41.720055,-87.751211'
     destination = 'grant park chicago il'
 
+    """
+    destination = input('What is the location of your event?').replace(" ","+")
+    origin = input('Where will you be traveling from?').replace(" ","+")
+    pattern = '%m.%d.%Y %H:%M'
+    date_time = input('What day and time will the event be? Military Time(MM.DD.YYYY HH:MM)')
+    arrival_time = int(time.mktime(time.strptime(date_time, pattern)))
+    """
+
 
 def sort_routes():
-    # returns an array of the closest subway stops by ditance (miles)
+    """
+
+    :return: an array of the closest subway stops by distance (miles)
+    """
+
     subway_stop_candidates = nearby_search_request(origin)
     # next three lines put lat long coordinates into lat,long|lat,long|lat,long... as standard
-    # in the  documentation for the link (see API doc. https://developers.google.com/maps/documentation/distance-matrix/intro)
+    # in the  documentation for the link
+    # (see API doc. https://developers.google.com/maps/documentation/distance-matrix/intro)
     bar_list_format_candidates = ''
     for x in subway_stop_candidates:
         bar_list_format_candidates = bar_list_format_candidates + x + '|'
@@ -57,15 +67,21 @@ def sort_routes():
 
     '''
     for x in subway_stop_candidates:
-    start_link = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
-    parameters_drive = 'origins=%s&destinations=%s&key=%s'%(origin,x,api_key)
-    parameters_transit = '&origins=%s&destinations=%s&key=%s'%(x,destination,api_key)
+        start_link = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
+        parameters_drive = 'origins=%s&destinations=%s&key=%s'%(origin,x,api_key)
+        parameters_transit = '&origins=%s&destinations=%s&key=%s'%(x,destination,api_key)
     '''
-    return subway_location
+    # TODO: FYI Steve I commented the below line, which was returning an error
+    # return subway_location
 
 
-# this function determines what time the user must arrive in order to catch their train
 def determine_arrival_time(middle_destination):
+    """
+    Function determines what time the user must arrive in order to catch their train
+    :param middle_destination: mode transfer station; this will eventually need to be computed
+    :return:
+    """
+
     start_link = 'https://maps.googleapis.com/maps/api/directions''/json?'
     end_link = '&mode=transit&transit_mode=subway'
     final_link = start_link + 'origin=%s&destination=%s&key=%s&arrival_time=%s' % (
@@ -74,12 +90,17 @@ def determine_arrival_time(middle_destination):
     json_total_routes = requests.get(final_link).json()
     # determines start time to get to destination and then adds 5 minute (300 sec) buffer, this is in unix form
     buffer_time = 300
-    arrival_ime_transit = json_total_routes['routes'][0]['legs'][0]['departure_time']['value'] - buffer_time
-    return arrival_ime_transit
+    arrival_time_transit = json_total_routes['routes'][0]['legs'][0]['departure_time']['value'] - buffer_time
+    return arrival_time_transit
 
 
-# this function will launch the directions for the most preferred path
 def launch_directions(arrival_time_transit, subway_stop_location):
+    """
+    Function will launch the directions for the most preferred path
+    :param arrival_time_transit:
+    :param subway_stop_location:
+    :return:
+    """
     # API for web browser generation https://developers.google.com/maps/documentation/urls/guide
     start_link = 'https://www.google.com/maps/dir/?api=1&'
     driving_link = start_link + 'origin=%s&destination=%s&travelmode=driving' % (origin, subway_stop_location)
@@ -92,8 +113,12 @@ def launch_directions(arrival_time_transit, subway_stop_location):
     webbrowser.open(transit_link)
 
 
-# documentation for API @ https://developers.google.com/places/web-service/search
 def nearby_search_request(location):
+    """
+    Documentation for API @ https://developers.google.com/places/web-service/search
+    :param location:
+    :return:
+    """
     start_link = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
     # (straight from documentation) A term to be matched against all content that Google has indexed for this place
     # including but not limited to name, type, and address, as well as customer reviews and other third-party content.
